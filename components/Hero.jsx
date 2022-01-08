5476c463d4cc8d3e54368e5e3956a1cf4fa5f481
cephaschapa <cephaschapa@gmail.com>
2022-01-08 21:14:46 +0200

    Title changes

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -9,10 +9,10 @@ const Hero = () => {
                 <div className="flex lg:items-center flex-col space-y-5">
                     <h1 className="font-bold lg:text-8xl text-center text-6xl text-white">Fast, reliable, and secure conferencing</h1>
                     <p className="text-gray-100 lg:text-2xl">Hold incredible events, share knowledge, build and grow your community, create opportunity.</p>
-                    <div className="flex flex-col space-y-4 lg:w-1/2">
-                        <button className="px-5 p-4 rounded-full w-full bg-green-600 text-white">Start a Meeting</button>
-                        <div className="">
-                            <form className="flex rounded-full bg-white">
+                    <div className="flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:w-1/2 lg:space-x-4">
+                        <button className="px-5 lg:w-1/2 h-16 rounded-full w-full bg-green-600 text-white">Start a Meeting</button>
+                        <div className="lg:w-1/2">
+                            <form className="flex rounded-full bg-white h-16 ">
                                 <div className="flex space-x-2 items-center w-full pl-5">
                                     <LockClosedIcon className="h-6 w-6 text-gray-500"/>
                                     <input className="outline-none w-full" placeholder="Meeting Code"/>
