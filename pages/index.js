e780c779cf0512a77fb9a6c869a3da532e4dccf2
cephaschapa <cephaschapa@gmail.com>
2022-01-09 10:29:31 +0200

    added more

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
-
 
 export default function Home() {
 
