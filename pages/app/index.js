286a8538d54588fba6549249df6ab8ce19de0208
cephaschapa <cephaschapa@gmail.com>
2022-01-23 19:23:56 +0200

    added background images

--- pages/app/index.js
+++ pages/app/index.js
@@ -5,6 +5,7 @@ import AppHeader from '../../components/AppComponents/AppHeader';
 import AppBottomNav from '../../components/AppComponents/AppBottomNav';
 import NavWrapper from "../../components/AppComponents/NavWrapper";
 import { ArrowRightIcon } from "@heroicons/react/outline";
+import { ClockIcon } from "@heroicons/react/solid";
 
 
 
@@ -36,19 +37,43 @@ const AccountPage = () =>{
                             </button>
                         </div>
                         <div className="snap-x mx-auto snap-mandatory flex overflow-scroll space-x-2">
-                            <div className="snap-start w-screen">
-                                <div className="flex items-center streamio-hero justify-center h-52 bg-gray-100 w-screen p-2 rounded-2xl">
-                                    Lorem ipsum
+                            <div className="snap-start w-screen ">
+                                <div className="flex items-center meeting justify-center h-52 bg-gray-100 w-screen rounded-2xl">
+                                    <div className="flex flex-col items-center justify-center bg-slate-800 p-3 rounded-2xl space-y-2">
+                                        <h3 className="text-white text-2xl font-bold uppercase">BISM App Tech Review</h3>
+                                        <div className="flex space-x-2 items-center justify-around">
+                                            
+                                            <p className="text-white">Starts - 10:45</p>
+                                            <p className="text-white">Ends - 10:45</p>
+                                        </div>
+                                        <button className="text-white p-3 rounded-full bg-green-600 uppercase font-bold text-sm">View Meeting Details</button>
+                                    </div>
                                 </div>
                             </div>
                             <div className="snap-start w-screen">
                                 <div className="flex items-center streamio-hero justify-center h-52 bg-gray-100 w-screen p-2 rounded-2xl">
-                                    Lorem ipsum
+                                <div className="flex flex-col items-center justify-center bg-green-600 p-3 rounded-2xl space-y-2">
+                                        <h3 className="text-white text-2xl font-bold uppercase">BISM App Tech Review</h3>
+                                        <div className="flex space-x-2 items-center justify-around">
+                                            
+                                            <p className="text-white">Starts - 10:45</p>
+                                            <p className="text-white">Ends - 10:45</p>
+                                        </div>
+                                        <button className="bg-white p-2 rounded-full text-green-600 uppercase font-bold text-sm">View Meeting Details</button>
+                                    </div>
                                 </div>
                             </div>
                             <div className="snap-start w-screen">
                                 <div className="flex items-center streamio-hero justify-center h-52 bg-gray-100 w-screen p-2 rounded-2xl">
-                                    Lorem ipsum
+                                <div className="flex flex-col items-center justify-center bg-green-600 p-3 rounded-2xl space-y-2">
+                                        <h3 className="text-white text-2xl font-bold uppercase">BISM App Tech Review</h3>
+                                        <div className="flex space-x-2 items-center justify-around">
+                                            
+                                            <p className="text-white">Starts - 10:45</p>
+                                            <p className="text-white">Ends - 10:45</p>
+                                        </div>
+                                        <button className="bg-white p-2 rounded-full text-green-600 uppercase font-bold text-sm">View Meeting Details</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -60,8 +85,16 @@ const AccountPage = () =>{
                         </div>
                         <div className="snap-x mx-auto snap-mandatory flex overflow-scroll space-x-2">
                             <div className="snap-start w-screen">
-                                <div className="flex items-center streamio-hero justify-center h-52 bg-gray-100 w-screen p-2 rounded-2xl">
-                                    Lorem ipsum
+                                <div className="flex items-center church justify-center h-52 bg-gray-100 w-screen p-2 rounded-2xl">
+                                <div className="flex flex-col items-center justify-center bg-slate-800 p-3 rounded-2xl space-y-2">
+                                        <h3 className="text-white text-2xl font-bold uppercase">EVENING SCIENCE CLASS</h3>
+                                        <div className="flex space-x-2 items-center justify-around">
+                                            
+                                            <p className="text-white">Starts - 19:45</p>
+                                            <p className="text-white">Ends - 20:45</p>
+                                        </div>
+                                        <button className="text-white p-3 rounded-full bg-green-600 uppercase font-bold text-sm">View Meeting Details</button>
+                                    </div>
                                 </div>
                             </div>
                             <div className="snap-start w-screen">
@@ -83,8 +116,16 @@ const AccountPage = () =>{
                         </div>
                         <div className="snap-x mx-auto snap-mandatory flex overflow-scroll space-x-2">
                             <div className="snap-start w-screen">
-                                <div className="flex items-center streamio-hero justify-center h-52 bg-gray-100 w-screen p-2 rounded-2xl">
-                                    Lorem ipsum
+                                <div className="flex items-center event justify-center h-52 bg-gray-100 w-screen p-2 rounded-2xl">
+                                <div className="flex flex-col items-center justify-center bg-slate-800 p-3 rounded-2xl space-y-2">
+                                        <h3 className="text-white text-2xl font-bold uppercase">VIRTUAL CHURCH SERVICE</h3>
+                                        <div className="flex space-x-2 items-center justify-around">
+                                            
+                                            <p className="text-white">Starts - 10:45</p>
+                                            <p className="text-white">Ends - 10:45</p>
+                                        </div>
+                                        <button className="bg-white p-2 rounded-full text-green-600 uppercase font-bold text-sm">View Meeting Details</button>
+                                    </div>
                                 </div>
                             </div>
                             <div className="snap-start w-screen">
