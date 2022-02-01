2be07028fd2cdd9a3e960c71582cddd82479595c
cephaschapa <cephaschapa@gmail.com>
2022-02-01 18:05:06 +0200

    added settings page

--- components/AppComponents/AppHeader.jsx
+++ components/AppComponents/AppHeader.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, {useEffect} from "react";
+import Portal from '@reach/portal';
+import { useRouter } from "next/router";
+import { useSession, signOut } from "next-auth/react";
+
+// Components and Reusables
+import Link from "next/link";
 import { DotsVerticalIcon, MenuIcon } from "@heroicons/react/outline";
 import NavWrapper from "./NavWrapper";
-import Portal from '@reach/portal';
-import Image from "next/image";
-import { useRouter } from "next/router";
 import { BadgeCheckIcon, BanIcon, CogIcon, CubeTransparentIcon, PencilIcon, ShieldCheckIcon, SupportIcon, ViewListIcon } from "@heroicons/react/solid";
-import { useSession, signOut } from "next-auth/react";
-import MainModal from "../Reusables/Modals";
-import {VscAccount, VscCombine, VscDebug, VscReferences, VscReport, VscSettings, VscSourceControl, VscWorkspaceTrusted} from 'react-icons/vsc'
+import {VscAccount, VscCombine, VscDebug, VscLinkExternal, VscMenu, VscReferences, VscReport, VscSearch, VscSettings, VscSourceControl, VscWorkspaceTrusted} from 'react-icons/vsc'
+import { useState } from "react";
 
-const AppHeader = ({session}) => {
-    const [isOpen, setIsOpen] = React.useState(false);
-    const [user, setUser] = React.useState({})
-    const [isSigningOut, setIsSigningOut] =  React.useState(false)
+const AppHeader = () => {
+    const [pathname, setpathname] = useState();
+    const authStatus = useSession();
 
-    const router = useRouter()
+    const {data: session} = authStatus;
+    console.log(session, authStatus)
+
+
+    const [isOpen, setIsOpen] = React.useState(false);   
+    const [isSigningOut, setIsSigningOut] =  React.useState(false);
+
+    const router = useRouter();
+    
+    useEffect(() => {
+      const {pathname} = router;
+      setpathname(pathname)
+      return () => {
+        pathname
+      };
+    }, []);
+    
     
     const image = '';
     const name =  '';
@@ -34,37 +51,25 @@ const AppHeader = ({session}) => {
     };
 
     const handleSignOut = () =>{  
-
       setIsSigningOut(true);
-      // router.push('/login')
-      setInterval(()=>{
-        
-        signOut();
-      },1000)
-      
-      
-    }
-
-    if(session){
-      
+      signOut();     
     }
 
     return(
-        <header className="shadow-md bg-green-600 fixed top-0 w-full">
-            
+        <header className="border-b bg-white fixed top-0 w-full">
             <NavWrapper>
                 <div className="flex justify-between w-full">
-                    <div className="">
+                    <div className="flex items-center justify-center">
                         <button type="button" aria-label="Drawer"  onClick={toggle}>
-                            <MenuIcon className="h-8 w-8 text-white"/>
+                            <VscMenu className="h-5 w-5 text-green-600"/>
                         </button>
                         
                     </div>
                     <div className="flex items-center">
                       <h1 className="text-xl font-bold uppercase text-white">Activities</h1>
                     </div>
-                    <div className="">
-                        <DotsVerticalIcon className="h-8 w-8 text-white"/>
+                    <div className="flex items-center justify-center">
+                        <VscSearch className="h-5 w-5 text-green-600"/>
                     </div>
                     {/* Drawer Menu */}
                     <Drawer isOpen={isOpen} toggle={toggle} position="left">
@@ -91,12 +96,16 @@ const AppHeader = ({session}) => {
                             </div>
                         </div>
                         <ul className="px-5  pb-5 pt-4 text-gray-700 space-y-4 lg:space-y-4 overflow-scroll">
-                          <li className="flex space-x-3 p-2 rounded-full transition duration-200 hover:bg-green-600 hover:text-white"><VscAccount className="h-6 w-6"/> <span className="font-bold">Edit Profile</span></li>
-                          <li className="flex space-x-3 p-2 rounded-full transition duration-200 hover:bg-green-600 hover:text-white"><VscSettings className="h-6 w-6"/> <span className="font-bold">Settings</span></li>
+                          <Link href="/app/profile" >
+                            <li className={` ${pathname === '/app/profile' ? 'bg-green-600 text-white': ''} flex space-x-3 p-2 rounded-full transition duration-200 hover:bg-green-600 hover:text-white`}><VscAccount className="h-6 w-6"/> <span className="font-bold">Edit Profile</span></li>
+                          </Link>
+                          <Link href="/app/settings">
+                            <li className={` ${pathname === '/app/settings' ? 'bg-green-600 text-white': ''} flex space-x-3 p-2 rounded-full transition duration-200 hover:bg-green-600 hover:text-white`}><VscSettings className="h-6 w-6"/> <span className="font-bold">Settings</span></li>
+                          </Link>
                           <li className="flex space-x-3 p-2 rounded-full transition duration-200 hover:bg-green-600 hover:text-white"><VscReport className="h-6 w-6"/> <span className="font-bold">Support</span></li>
                           <li className="flex space-x-3 p-2 rounded-full transition duration-200 hover:bg-green-600 hover:text-white"><VscCombine className="h-6 w-6"/> <span className="font-bold">Feature Request</span></li>
-                          <li className="flex space-x-3 p-2 rounded-full transition duration-200 hover:bg-green-600 hover:text-white"><VscWorkspaceTrusted className="h-6 w-6"/> <span className="font-bold">Privacy Policy</span></li>
-                          <li className="flex space-x-3 p-2 rounded-full transition duration-200 hover:bg-green-600 hover:text-white"><VscReferences className="h-6 w-6"/> <span className="font-bold">Terms of Service</span></li>                        
+                          <li className="flex items-center space-x-3 p-2 rounded-full transition duration-200 hover:bg-green-600 hover:text-white"><VscWorkspaceTrusted className="h-6 w-6"/> <span className="font-bold">Privacy Policy </span><VscLinkExternal className="h-3 w-3"/></li>
+                          <li className="flex items-center space-x-3 p-2 rounded-full transition duration-200 hover:bg-green-600 hover:text-white"><VscReferences className="h-6 w-6"/> <span className="font-bold">Terms of Service </span><VscLinkExternal className="h-3 w-3"/></li>
                         </ul>
                         <div className="flex px-5 w-full">
                           <button className="bg-green-600 p-2 rounded-full text-white font-bold w-full" onClick={handleSignOut}>{
---
+++ pages/app/profile.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+// Components and Reusables
+import AppBottomNav from "../../components/AppComponents/AppBottomNav";
+import AppHeader from "../../components/AppComponents/AppHeader";
+import NavWrapper from "../../components/AppComponents/NavWrapper";
+
+const ProfilePage = () => {
+    return(
+        <>
+            <AppHeader />
+            <NavWrapper>
+            </NavWrapper>
+            <AppBottomNav />
+            
+        </>
+    )
+}
+
+export default ProfilePage;
