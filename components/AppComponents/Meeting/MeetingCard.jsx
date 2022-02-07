6bc4b16a83198e947964cf3d669fbed363fc117f
cephaschapa <cephaschapa@gmail.com>
2022-02-07 23:05:34 +0200

    Added meetings card

--- components/AppComponents/Meeting/MeetingCard.jsx
+++ components/AppComponents/Meeting/MeetingCard.jsx
@@ -1,14 +1,21 @@
+import { CalendarIcon } from "@heroicons/react/outline";
 import { VscWatch } from "react-icons/vsc";
 
 const MeetingCard = ({title, time}) => {
     return(
-        <div className="">
-            <p className="font-bold">{title}</p>
-            <div className="flex items-center">
-                <VscWatch className="h-4 w-4"/>
-                <p>{time}</p>
+        <div className="flex space-x-3 items-center w-full bg-slate-100 rounded-2xl p-3">
+            <div className="p-3 bg-green-600 h-full rounded-2xl">
+                <CalendarIcon className="h-6 w-6 text-white"/>
+            </div>
+            <div className="">
+                <p className="font-bold">{title}</p>
+                <div className="flex items-center">
+                    <VscWatch className="h-4 w-4"/>
+                    <p>{time}</p>
+                </div>
             </div>
         </div>
+        
     )
 }
 
--- pages/meeting.js
+++ pages/meeting.js
@@ -5,11 +5,15 @@ import AppBottomNav from "../components/AppComponents/AppBottomNav";
 import { VscChecklist, VscPackage, VscWatch } from "react-icons/vsc";
 import { CalendarIcon } from "@heroicons/react/outline";
 import MeetingCard from "../components/AppComponents/Meeting/MeetingCard";
+import Head from "next/head";
 
 
 const Meeting = () => {
     return(
         <div>
+            <Head>
+                <title>StreamIO - Meeting</title>
+            </Head>
             <AppHeader pageName="Meeting"/>
             <NavWrapper>
                 <div className="flex flex-col space-y-3 mt-12 w-full py-3">
@@ -17,14 +21,17 @@ const Meeting = () => {
                         <button className="w-1/2 text-white font-bold bg-green-600 p-3 rounded-full">Create a Meeting</button>
                         <button className="w-1/2 border-2 border-green-600 font-bold text-green-600 p-3 rounded-full">Join a Meeting</button>
                     </div>
-                    <div className="flex flex-col space-y-3 border-t-2 py-3">
+                    <div className="flex flex-col space-y-3 border-t-2 py-3 pb-16">
                         <p className="font-bold">Your upcoming meetings</p>
-                        <div className="flex space-x-3 items-center w-full bg-slate-100 rounded-2xl p-3">
-                            <div className="p-3 bg-green-600 h-full rounded-2xl">
-                                <CalendarIcon className="h-6 w-6 text-white"/>
-                            </div>
-                            <MeetingCard title="Cephas Chapa - Spa Massage" time="Tue, 19:00-19:45"/>
-                        </div>
+                        
+                        <MeetingCard title="Cephas Chapa - Online Yoga" time="Tue, 19:00-19:45"/>
+                        <MeetingCard title="StreamIO Tech Review" time="Mon, 08:00-08:30"/>
+                        <MeetingCard title="Microsoft StreamIO Aquisition" time="Wed, 10:00-13:00"/>
+                        <MeetingCard title="Church Fellowship" time="Fri, 19:00-19:45"/>
+                        <MeetingCard title="Cephas Chapa - Online Yoga agin" time="Fri, 19:00-19:45"/>
+                        <MeetingCard title="Church Fellowship" time="Fri, 19:00-19:45"/>
+                        <MeetingCard title="Cephas Chapa - Online Yoga agin" time="Fri, 19:00-19:45"/>
+                        <MeetingCard title="Cephas Chapa - Online Yoga agin" time="Fri, 19:00-19:45"/>
                     </div>
                 </div>
             </NavWrapper>
