07a3afc2b91d1ec817e7160ed4c92f805c6fbadd
cephaschapa <cephaschapa@gmail.com>
2022-02-10 16:09:55 +0200

    join config ui

--- pages/app/join-meeting.js
+++ pages/app/join-meeting.js
@@ -1,9 +1,19 @@
+import React, {useState, useEffect} from "react";
+import { useRouter } from "next/router";
+
 import { VscCode, VscKey, VscPass, VscSymbolNumeric, VscTerminalTmux, VscWindow } from "react-icons/vsc";
 import AppHeader from "../../components/AppComponents/AppHeader";
 import MeetingActionButtons2 from "../../components/AppComponents/Meeting/MeetingActionButton2";
 import NavWrapper from "../../components/AppComponents/NavWrapper";
 
 const JoinRoom = () => {
+    const router = useRouter();
+    const [meetingCode, setMeetingCode] = useState('wfmr-4icb-1l8c')
+
+    const handleJoinMeeting = (e) => {
+        e.preventDefault()
+        router.push(`/app/meeting-room/${meetingCode}`)
+    }
     return(
         <div className="dark:bg-slate-800 h-screen">
             <AppHeader pageName="Join a Meeting"/>
@@ -16,9 +26,9 @@ const JoinRoom = () => {
                             <p className="font-bold dark:text-slate-200">Enter the meeting code <span className="text-red-500">*</span></p>
                             <div className="flex space-x-3 items-center w-full bg-slate-100 dark:bg-slate-700 p-3 rounded-full">
                                 <VscKey className="h-5 w-5 dark:text-slate-200"/>
-                                <input className="outline-none bg-transparent dark:text-slate-200" type="text" placeholder="wfmr-4icb-1l8x"/>
+                                <input value={meetingCode} onChange={(e)=>setMeetingCode(e.target.value)} className="outline-none bg-transparent dark:text-slate-200" type="text" placeholder="wfmr-4icb-1l8x"/>
                             </div>
-                            <button className="w-full font-bold text-white p-3 bg-green-600 rounded-full">Join Meeting</button>
+                            <button onClick={handleJoinMeeting} className="w-full font-bold text-white p-3 bg-green-600 rounded-full">Join Meeting</button>
                         </form>
                     </div>
                 </div>
---
+++ pages/app/meeting-room/[roomid].js
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from "react";
+import { useRouter } from "next/router";
+import { VscArrowLeft, VscDeviceCameraVideo, VscRecord } from "react-icons/vsc";
+import { MicrophoneIcon } from "@heroicons/react/outline";
+
+const MeetingRoom = () => {
+
+    const router = useRouter()
+    console.log(router.query.roomid)
+    return(
+        <div className="dark:bg-slate-800 h-screen p-3">
+            <div>
+                <button onClick={()=>router.back()} className="p-3 border border-slate-200 rounded-full">
+                    <VscArrowLeft className="h-5 w-5 text-white"/>
+                </button>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+                <div className="flex justify-center w-full py-6">
+                    <div className="w-1/2 h-72 bg-slate-600 rounded-2xl">
+
+                    </div>
+                </div>
+                <div className="flex space-x-3">
+                    <button className="p-4 rounded-full bg-slate-600">
+                        <VscDeviceCameraVideo className="h-6 w-6 text-white"/>
+                    </button>
+                    <button className="p-4 rounded-full bg-slate-600">
+                        <MicrophoneIcon className="h-6 w-6 text-white"/>
+                    </button>
+                    <button className="p-4 rounded-full bg-slate-600">
+                        <VscRecord className="h-6 w-6 text-white"/>
+                    </button>
+                </div>
+                <div className="w-full flex items-center justify-center py-6">
+                    <button className="p-3 bg-green-600 w-[100px] rounded-full font-bold text-white">
+                        Join
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MeetingRoom;
