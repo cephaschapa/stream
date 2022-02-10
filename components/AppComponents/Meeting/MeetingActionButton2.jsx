c7c055adc613aa18f3faca0e9eead3c90240969a
cephaschapa <cephaschapa@gmail.com>
2022-02-10 15:22:40 +0200

    ...

--- components/AppComponents/Meeting/MeetingActionButton2.jsx
+++ components/AppComponents/Meeting/MeetingActionButton2.jsx
@@ -5,7 +5,7 @@ import { VscArrowLeft } from "react-icons/vsc";
 
 const MeetingActionButtons2 = ({url, btnText}) => {
     return(
-        <div className="flex items-center justify-between w-full relative top-16 px-3 z-0">
+        <div className="flex items-center bg-white py-3 dark:bg-slate-800 justify-between w-full relative top-16 px-3 z-0">
             <div>
                 <button className="p-3 border rounded-full" onClick={()=>Router.push('/app/meeting')}>
                     <VscArrowLeft className="h-5 w-5 dark:text-slate-200"/>
